66a3cb5ad1fde5925d29555dcd94765146edeeb2
ClaireHarned <charned@wisc.edu>
2023-04-24 12:39:56 -0500

    update retrieve

--- js/sophie-example.js
+++ js/sophie-example.js
@@ -20,32 +20,7 @@ var map;
       getData(map);
   };
 
-  //function to retrieve the data and place it on the map
-function getData(map){
-  //load the data, then map
-  fetch("data/made.geojson")
-      .then(function(response){
-          return response.json();
-      })
-      .then(function(json){
-          //create a Leaflet GeoJSON layer and add it to the map
-          L.geoJson(json, {
-              //use filter function to only show statue names
-              filter: function(feature, layer) {
-                  return feature.properties;
-              },
-              // use pointToLayer option to add custom icons
-              pointToLayer: function (feature, latlng) {
-                  var customIcon = L.icon({
-                      iconUrl: 'img/statue.svg',
-                      iconSize: [40, 40], // size of the icon
-                      iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-                  });
-                  return L.marker(latlng, { icon: customIcon });
-              }
-          }).addTo(map);
-      })
-};
+
 
 //function to attach popups to each mapped feature
 function onEachFeature(feature, layer) {
@@ -74,5 +49,31 @@ function getData(map){
             }).addTo(map);
         })  
 };
+  //function to retrieve the data and place it on the map
+  function getData(map){
+    //load the data, then map
+    fetch("data/made.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                //use filter function to only show statue names
+                filter: function(feature, layer) {
+                    return feature.properties;
+                },
+                // use pointToLayer option to add custom icons
+                pointToLayer: function (feature, latlng) {
+                    var customIcon = L.icon({
+                        iconUrl: 'img/statue.svg',
+                        iconSize: [40, 40], // size of the icon
+                        iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+                    });
+                    return L.marker(latlng, { icon: customIcon });
+                }
+            }).addTo(map);
+        })
+  };
 
   document.addEventListener('DOMContentLoaded',createMap)
