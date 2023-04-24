c0750c76d72db56b7d27b48548275275503603e0
ClaireHarned <charned@wisc.edu>
2023-04-24 12:39:07 -0500

    retrieve update

--- js/sophie-example.js
+++ js/sophie-example.js
@@ -47,4 +47,32 @@ function getData(map){
       })
 };
 
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    fetch("data/made.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        })  
+};
+
   document.addEventListener('DOMContentLoaded',createMap)
