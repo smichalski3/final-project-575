80d6471db26b3c979b047c245e8e3cd225386c13
smichalski3 <smichalski0302@gmail.com>
2023-04-24 21:15:20 -0500

    adding second map

---
+++ js/example.js
@@ -0,0 +1,79 @@
+// Initialize the maps
+var statueMap = L.map('statueMap', {
+    center: [39, 25],
+    zoom: 6,
+    maxZoom: 12,
+    minZoom: 4,
+    scrollWheelZoom: false,
+    zoomControl: true,
+    maxBounds: [
+        [50, 50],
+        [25, 0]
+    ],
+});
+
+var museumMap = L.map('museumMap', {
+    center: [39, 25],
+    zoom: 6,
+    maxZoom: 12,
+    minZoom: 4,
+    scrollWheelZoom: false,
+    zoomControl: true,
+    maxBounds: [
+        [50, 50],
+        [25, 0]
+    ],
+});
+
+// Add OSM base tilelayer to the maps
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+    maxZoom: 18,
+}).addTo(statueMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+    maxZoom: 18,
+}).addTo(museumMap);
+
+// Call getData function for each map
+getData(statueMap, 'data/statues.geojson', 'img/statue.svg');
+getData(museumMap, 'data/museums.geojson', 'img/greek-column.svg');
+
+// function to retrieve the data and place it on the map
+function getData(map, url, iconUrl) {
+    // load the data, then map
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            // create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                // use filter function to only show names
+                filter: function(feature, layer) {
+                    return feature.properties;
+                },
+                // use pointToLayer option to add custom icons
+                pointToLayer: function(feature, latlng) {
+                    var customIcon = L.icon({
+                        iconUrl: iconUrl,
+                        iconSize: [40, 40], // size of the icon
+                        iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+                    });
+                    return L.marker(latlng, { icon: customIcon });
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        })
+};
+
+// function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    var popupContent = "";
+    if (feature.properties) {
+        // loop to add feature property names and values to html string
+        for (var property in feature.properties) {
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+var statueMap;
 
-var map;
+var museumMap;
 
-// function to instantiate the Leaflet map
-function createMap(){
+// Statue Map
+
+//function to instantiate the statue map
+function createStatueMap(){
     //create the map
-    map = L.map('map', {
+    statueMap = L.map('statueMap', {
         //map parameters
         center: [39, 25],
         zoom: 6,
@@ -22,16 +25,16 @@ function createMap(){
   // add OSM base tilelayer
   L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
     maxZoom: 18,
-  }).addTo(map);
+  }).addTo(statueMap);
 
   // call getData function
-  getData(map);
+  getData(statueMap);
 };
 
 // function to retrieve the data and place it on the map
-function getData(map) {
+function getData(statueMap) {
   // load the data, then map
-  fetch("data/museums.geojson")
+  fetch("data/statues.geojson")
     .then(function (response) {
       return response.json();
     })
@@ -45,14 +48,14 @@ function getData(map) {
         // use pointToLayer option to add custom icons
         pointToLayer: function (feature, latlng) {
           var customIcon = L.icon({
-            iconUrl: 'img/greek-column.svg',
+            iconUrl: 'img/statue.svg',
             iconSize: [40, 40], // size of the icon
             iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
           });
           return L.marker(latlng, { icon: customIcon });
         },
         onEachFeature: onEachFeature
-      }).addTo(map);
+      }).addTo(statueMap);
     })
 };
 
@@ -69,6 +72,78 @@ function onEachFeature(feature, layer) {
   };
 };
 
+
+
+
+
+
+
+
+// Museum Map
+
+// function to instantiate the Leaflet map
+function createMuseumMap() {
+  // create the map
+  museumMap = L.map('museumMap', {
+    center: [39, 25],
+    zoom: 6,
+    // maxZoom: 6,  // set maxZoom and minZoom to the same value to disable zooming
+    // minZoom: 6
+  });
+
+  // add OSM base tilelayer
+  L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+    maxZoom: 18,
+  }).addTo(museumMap);
+
+  // call getData function
+  getData(museumMap);
+};
+
+// function to retrieve the data and place it on the map
+function getData(museumMap) {
+  // load the data, then map
+  fetch("data/museums.geojson")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      // create a Leaflet GeoJSON layer and add it to the map
+      L.geoJson(json, {
+        // use filter function to only show museum names
+        filter: function (feature, layer) {
+          return feature.properties;
+        },
+        // use pointToLayer option to add custom icons
+        pointToLayer: function (feature, latlng) {
+          var customIcon = L.icon({
+            iconUrl: 'img/greek-column.svg',
+            iconSize: [40, 40], // size of the icon
+            iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+          });
+          return L.marker(latlng, { icon: customIcon });
+        },
+        onEachFeature: onEachFeature
+      }).addTo(museumMap);
+    })
+};
+
+// function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+  // no property named popupContent; instead, create html string with all properties
+  var popupContent = "";
+  if (feature.properties) {
+    // loop to add feature property names and values to html string
+    for (var property in feature.properties) {
+      popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+    }
+    layer.bindPopup(popupContent);
+  };
+};
+
+
 // call createMap function when the DOM is loaded
-document.addEventListener('DOMContentLoaded', createMap);
+document.addEventListener('DOMContentLoaded', createStatueMap);
+document.addEventListener('DOMContentLoaded', createMuseumMap);
+
 
