f8fba2f5b53fa23aac39ce1046bc1c0375b73334
smichalski3 <smichalski0302@gmail.com>
2023-05-01 16:49:54 -0500

    adding time period maps and text changes

--- js/example.js
+++ js/example.js
@@ -1,147 +1,69 @@
-// Initialize the maps
-var statueMap = L.map('statueMap', {
+
+// Museum Map
+
+// function to instantiate the Leaflet map
+function createMuseumMap() {
+  // create the map
+  museumMap = L.map('museumMap', {
     center: [39, 25],
     zoom: 6,
-    maxZoom: 12,
-    minZoom: 4,
-    scrollWheelZoom: false,
-    zoomControl: true,
-    maxBounds: [
-        [50, 50],
-        [25, 0]
-    ],
-});
+    // maxZoom: 6,  // set maxZoom and minZoom to the same value to disable zooming
+    // minZoom: 6
+  });
 
-var museumMap = L.map('museumMap', {
-    center: [39, 25],
-    zoom: 5,
-    maxZoom: 12,
-    minZoom: 4,
-    scrollWheelZoom: false,
-    zoomControl: true,
-    maxBounds: [
-        [50, 20],
-        [25, 0]
-    ],
-});
-
-var romanMap = L.map('romanMap', {
-    center: [39, 25],
-    zoom: 6,
-    maxZoom: 12,
-    minZoom: 4,
-    scrollWheelZoom: false,
-    zoomControl: true,
-    maxBounds: [
-        [50, 50],
-        [25, 0]
-    ],
-
-});
-
-var movementMap = L.map('movementMap', {
-    center: [39, 25],
-    zoom: 5,
-    maxZoom: 12,
-    minZoom: 4,
-    scrollWheelZoom: false,
-    zoomControl: true,
-
-});
-
-
-// Add base tilelayer to the maps
-L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  // add OSM base tilelayer
+  L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
     maxZoom: 18,
-}).addTo(statueMap);
+  }).addTo(museumMap);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
-    maxZoom: 18,
-}).addTo(museumMap);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
-    maxZoom: 18,
-}).addTo(romanMap);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
-    maxZoom: 18,
-}).addTo(movementMap);
-
-// Call getData function for each map
-getData(statueMap, 'data/statues.geojson', 'img/statue.svg');
-getData(museumMap, 'data/museums.geojson', 'img/greek-column.svg');
-getData(romanMap, 'data/romans.geojson', 'img/roman-statue.svg');
-getData(romanMap, 'data/statues.geojson', 'img/statue.svg');
-getData(movementMap, 'data/movement-lines.geojson');
-getData(movementMap, 'data/statues.geojson', 'img/statue.svg');
-getData(movementMap, 'data/museums.geojson', 'img/greek-column.svg');
-
-
-function getData(map, url, iconUrl) {
-    // load the data, then add it to the map
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            // create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json, {
-                // use filter function to only show names
-                filter: function(feature, layer) {
-                    return feature.properties;
-                },
-                // use pointToLayer option to add custom icons
-                pointToLayer: function(feature, latlng) {
-                    var customIcon = L.icon({
-                        iconUrl: iconUrl,
-                        iconSize: [40, 40], // size of the icon
-                        iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-                    });
-                    var marker = L.marker(latlng, { icon: customIcon });
-                    marker.on('mouseover', function() {
-                        var popup = L.popup()
-                            .setLatLng(latlng)
-                            .setContent(feature.properties.name) // change this to customize the popup content
-                            .openOn(map);
-                    });
-                    return marker;
-                },
-                style: style,
-                onEachFeature: onEachFeature
-            }).addTo(map);
-        })
+  // call getData function
+  getData(museumMap);
 };
 
-function style(feature) {
-    return {
-        weight: 2,
-        opacity: 1,
-        color: '#787878',
-        dashArray: '0',
-        fillOpacity: 0.7
-    };
-}
+// function to retrieve the data and place it on the map
+function getData(museumMap) {
+  // load the data, then map
+  fetch("data/museums.geojson")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      // create a Leaflet GeoJSON layer and add it to the map
+      L.geoJson(json, {
+        // use filter function to only show museum names
+        filter: function (feature, layer) {
+          return feature.properties;
+        },
+        // use pointToLayer option to add custom icons
+        pointToLayer: function (feature, latlng) {
+          var customIcon = L.icon({
+            iconUrl: 'img/greek-column.svg',
+            iconSize: [40, 40], // size of the icon
+            iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+          });
+          return L.marker(latlng, { icon: customIcon });
+        },
+        onEachFeature: onEachFeature
+      }).addTo(museumMap);
+    })
+};
 
 // function to attach popups to each mapped feature
 function onEachFeature(feature, layer) {
-    var popupContent = "";
-    if (feature.properties) {
-        // loop to add feature property names and values to html string
-        for (var property in feature.properties) {
-            if (property != "image"){
-                popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-            }
-            else{
-                popupContent += "<img id='test' src='" + feature.properties[property] + "'>";
-            }
-        }
-    // bind the popup to the layer, and show it on hover
-    layer.bindPopup(popupContent, { closeButton: false, offset: L.point(0, -10) });
-    layer.on('mouseover', function(e) {
-        this.openPopup();
-    });
-    layer.on('mouseout', function(e) {
-        this.closePopup();
-    });
-    };
+  // no property named popupContent; instead, create html string with all properties
+  var popupContent = "";
+  if (feature.properties) {
+    // loop to add feature property names and values to html string
+    for (var property in feature.properties) {
+      popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+    }
+    layer.bindPopup(popupContent);
+  };
 };
+
+
+// call createMap function when the DOM is loaded
+document.addEventListener('DOMContentLoaded', createStatueMap);
+document.addEventListener('DOMContentLoaded', createMuseumMap);
+
+
--- js/main.js
+++ js/main.js
@@ -1,149 +1,205 @@
-var statueMap;
+// Initialize the maps
+var statueMap = L.map('statueMap', {
+  center: [39, 25],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 50],
+      [25, 0]
+  ],
+});
 
-var museumMap;
+var archaicMap = L.map('archaicMap', {
+  center: [39, 25],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 50],
+      [25, 0]
+  ],
+});
 
-// Statue Map
+var classicalMap = L.map('classicalMap', {
+  center: [39, 25],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 50],
+      [25, 0]
+  ],
+});
 
-//function to instantiate the statue map
-function createStatueMap(){
-    //create the map
-    statueMap = L.map('statueMap', {
-        //map parameters
-        center: [39, 25],
-        zoom: 6,
-        maxZoom: 12,
-        minZoom: 4,
-        scrollWheelZoom: false,
-        zoomControl: true,
-        //constrain pan to data
-        maxBounds: [
-            [50, 50],
-            [25, 0]
-            ],
-    });
+var hellenisticMap = L.map('hellenisticMap', {
+  center: [39, 25],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 50],
+      [25, 0]
+  ],
+});
 
-  // add OSM base tilelayer
-  L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
-    maxZoom: 18,
-  }).addTo(statueMap);
+var museumMap = L.map('museumMap', {
+  center: [39, 25],
+  zoom: 5,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 20],
+      [25, 0]
+  ],
+});
 
-  // call getData function
-  getData(statueMap);
+var romanMap = L.map('romanMap', {
+  center: [39, 25],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+  maxBounds: [
+      [50, 50],
+      [25, 0]
+  ],
+
+});
+
+var movementMap = L.map('movementMap', {
+  center: [39, 25],
+  zoom: 5,
+  maxZoom: 12,
+  minZoom: 4,
+  scrollWheelZoom: false,
+  zoomControl: true,
+
+});
+
+
+// Add base tilelayer to the maps
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(statueMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(archaicMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(classicalMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(hellenisticMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(museumMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(romanMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+  maxZoom: 18,
+}).addTo(movementMap);
+
+// Call getData function for each map
+getData(statueMap, 'data/greek-statues.geojson', 'img/statue.svg');
+getData(archaicMap, 'data/archaic-statues.geojson', 'img/statue.svg');
+getData(classicalMap, 'data/archaic-statues.geojson', 'img/statue.svg');
+getData(classicalMap, 'data/classical-statues.geojson', 'img/statue.svg');
+getData(hellenisticMap, 'data/archaic-statues.geojson', 'img/statue.svg');
+getData(hellenisticMap, 'data/classical-statues.geojson', 'img/statue.svg');
+getData(hellenisticMap, 'data/hellenistic-statues.geojson', 'img/statue.svg');
+getData(museumMap, 'data/museums.geojson', 'img/greek-column.svg');
+getData(romanMap, 'data/roman-statues.geojson', 'img/roman-statue.svg');
+getData(romanMap, 'data/greek-statues.geojson', 'img/statue.svg');
+getData(movementMap, 'data/movement-lines.geojson');
+getData(movementMap, 'data/greek-statues.geojson', 'img/statue.svg');
+getData(movementMap, 'data/museums.geojson', 'img/greek-column.svg');
+
+
+function getData(map, url, iconUrl) {
+  // load the data, then add it to the map
+  fetch(url)
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(json) {
+          // create a Leaflet GeoJSON layer and add it to the map
+          L.geoJson(json, {
+              // use filter function to only show names
+              filter: function(feature, layer) {
+                  return feature.properties;
+              },
+              // use pointToLayer option to add custom icons
+              pointToLayer: function(feature, latlng) {
+                  var customIcon = L.icon({
+                      iconUrl: iconUrl,
+                      iconSize: [40, 40], // size of the icon
+                      iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+                  });
+                  var marker = L.marker(latlng, { icon: customIcon });
+                  marker.on('mouseover', function() {
+                      var popup = L.popup()
+                          .setLatLng(latlng)
+                          .setContent(feature.properties.name) // change this to customize the popup content
+                          .openOn(map);
+                  });
+                  return marker;
+              },
+              style: style,
+              onEachFeature: onEachFeature
+          }).addTo(map);
+      })
 };
 
-// function to retrieve the data and place it on the map
-function getData(statueMap) {
-  // load the data, then map
-  fetch("data/statues.geojson")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      // create a Leaflet GeoJSON layer and add it to the map
-      L.geoJson(json, {
-        // use filter function to only show statue names
-        filter: function (feature, layer) {
-          return feature.properties;
-        },
-        // use pointToLayer option to add custom icons
-        pointToLayer: function (feature, latlng) {
-          var customIcon = L.icon({
-            iconUrl: 'img/statue.svg',
-            iconSize: [40, 40], // size of the icon
-            iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-          });
-          return L.marker(latlng, { icon: customIcon });
-        },
-        onEachFeature: onEachFeature
-      }).addTo(statueMap);
-    })
-};
+function style(feature) {
+  return {
+      weight: 2,
+      opacity: 1,
+      color: '#787878',
+      dashArray: '0',
+      fillOpacity: 0.7
+  };
+}
 
 // function to attach popups to each mapped feature
 function onEachFeature(feature, layer) {
-  // no property named popupContent; instead, create html string with all properties
   var popupContent = "";
   if (feature.properties) {
-    // loop to add feature property names and values to html string
-    for (var property in feature.properties) {
-      popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-    }
-    layer.bindPopup(popupContent);
-  };
-};
-
-
-
-
-
-
-
-
-// Museum Map
-
-// function to instantiate the Leaflet map
-function createMuseumMap() {
-  // create the map
-  museumMap = L.map('museumMap', {
-    center: [39, 25],
-    zoom: 6,
-    // maxZoom: 6,  // set maxZoom and minZoom to the same value to disable zooming
-    // minZoom: 6
+      // loop to add feature property names and values to html string
+      for (var property in feature.properties) {
+          if (property != "image"){
+              popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+          }
+          else{
+              popupContent += "<img id='test' src='" + feature.properties[property] + "'>";
+          }
+      }
+  // bind the popup to the layer, and show it on hover
+  layer.bindPopup(popupContent, { closeButton: false, offset: L.point(0, -10) });
+  layer.on('mouseover', function(e) {
+      this.openPopup();
   });
-
-  // add OSM base tilelayer
-  L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
-    maxZoom: 18,
-  }).addTo(museumMap);
-
-  // call getData function
-  getData(museumMap);
-};
-
-// function to retrieve the data and place it on the map
-function getData(museumMap) {
-  // load the data, then map
-  fetch("data/museums.geojson")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      // create a Leaflet GeoJSON layer and add it to the map
-      L.geoJson(json, {
-        // use filter function to only show museum names
-        filter: function (feature, layer) {
-          return feature.properties;
-        },
-        // use pointToLayer option to add custom icons
-        pointToLayer: function (feature, latlng) {
-          var customIcon = L.icon({
-            iconUrl: 'img/greek-column.svg',
-            iconSize: [40, 40], // size of the icon
-            iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-          });
-          return L.marker(latlng, { icon: customIcon });
-        },
-        onEachFeature: onEachFeature
-      }).addTo(museumMap);
-    })
-};
-
-// function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-  // no property named popupContent; instead, create html string with all properties
-  var popupContent = "";
-  if (feature.properties) {
-    // loop to add feature property names and values to html string
-    for (var property in feature.properties) {
-      popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-    }
-    layer.bindPopup(popupContent);
+  layer.on('mouseout', function(e) {
+      this.closePopup();
+  });
   };
 };
-
-
-// call createMap function when the DOM is loaded
-document.addEventListener('DOMContentLoaded', createStatueMap);
-document.addEventListener('DOMContentLoaded', createMuseumMap);
-
 
