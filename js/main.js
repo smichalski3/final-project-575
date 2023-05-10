0663d5ee4c24801dfca54c5b3b271f770da7fa75
smichalski3 <smichalski0302@gmail.com>
2023-05-09 19:28:43 -0500

    updating background image sizes

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@ var finalMap = L.map('finalMap', {
 
 
 // Add base tilelayer to the maps
-L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clgpx6cap00e901nn9jbi9fyt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
+L.tileLayer('https://api.mapbox.com/styles/v1/smichalski/clhgusc5500m701pef5hadewk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21pY2hhbHNraSIsImEiOiJjbDl6d2s0enYwMnI1M29uMDhzNXB0NTRlIn0.c1_vy157AkEEGNIfyQI9YQ', {
   maxZoom: 18,
 }).addTo(blankMap);
 
